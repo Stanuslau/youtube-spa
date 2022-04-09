44576e98e5d8564b1da9ff948a4e10a5c64693fa
Stanislau Trebukhouski <stanislav.trebuhovskiy@drupalsquad.com>
2022-04-09 12:54:21 +0300

    Auth page initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,92 @@
+import { Row, Col, Form, Input, Button, Checkbox } from "antd";
+import { Layout } from "antd";
+
+function Login() {
+  const { Header, Footer, Sider, Content } = Layout;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    // <Row type="flex" justify="center" align="center">
+
+    <Row
+      type="flex"
+      justify="center"
+      align="middle"
+      style={{ minHeight: "100vh" }}
+    >
+      <Col span={4}>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  );
+}
+
+export default Login;
