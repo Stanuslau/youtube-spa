9abff2eeae5bc155f18a62dd527905456036bffe
Stanislau Trebukhouski <stanislav.trebuhovskiy@drupalsquad.com>
2022-04-26 18:49:14 +0300

    Parsing videos array

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,29 +7,41 @@ const { Search } = Input;
 
 function Main() {
   const [searchValue, setSearchValue] = useState("");
+
   function onChangeFunc(event) {
     setSearchValue(event.target.value);
   }
-  const onSearch = async (value) => {
-    // let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${value}&key=AIzaSyAu1EVzXZ7gy-eoV0k3BLmNdnc2vK3xTPg`;
-    console.log("Next is axios run");
-    // axios асинхронный запрос, как закончится, так и отработает.
-    let videosList = await axios
-      .get(
-        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=AIzaSyAu1EVzXZ7gy-eoV0k3BLmNdnc2vK3xTPg`
-      )
-      .then((res) => {
-        console.log(res);
-      });
 
-    let videoStatistic = await axios
-      .get(
-        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=leiXBeRHGcE&key=AIzaSyAu1EVzXZ7gy-eoV0k3BLmNdnc2vK3xTPg`
-      )
-      .then((res) => {
-        console.log(res);
-      });
-    console.log("Axios run finished");
+  const onSearch = async (value) => {
+    //q=surfing by default for troubleshooting
+
+    async function getVideosList() {
+      let data = await axios
+        .get(
+          `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=${value}&key=AIzaSyAu1EVzXZ7gy-eoV0k3BLmNdnc2vK3xTPg`
+        )
+        .then((res) => {
+          return res.data.items;
+        });
+      return data;
+    }
+
+    async function getOneVideoStatistic(videoID) {
+      let videoStatistic = await axios
+        .get(
+          `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=${videoID}&key=AIzaSyAu1EVzXZ7gy-eoV0k3BLmNdnc2vK3xTPg`
+        )
+        .then((res) => {
+          console.log(res);
+        });
+    }
+    let videosArray = await getVideosList().then((res) => res);
+    videosArray.map((item) => {
+      console.log("item = ", item);
+      console.log("item.id.videoId = ", item.id.videoId);
+      console.log("item.snippet.title = ", item.snippet.title);
+      console.log("----------");
+    });
   };
 
   return (
