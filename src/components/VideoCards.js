2233f4ad9e2410f3e75da361e95a7d5c9f7a17a0
Stanislau Trebukhouski <stanislav.trebuhovskiy@drupalsquad.com>
2022-05-10 07:40:51 +0300

    Favourites add button configuration

--- src/components/VideoCards.js
+++ src/components/VideoCards.js
@@ -7,7 +7,6 @@ const { Meta } = Card;
 
 const VideoCards = (props) => {
   // почему не работает const [videos, setVideos] = useState(props.videos);
-  const [counter, setCounter] = useState(0);
   const [form] = Form.useForm();
   const [favourites, setFavourites] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -66,11 +65,12 @@ const VideoCards = (props) => {
   });
 
   const showModal = () => {
+    // how to render Form.item https://github.com/ant-design/ant-design/issues/22421#issuecomment-778403975
     form.setFieldsValue({
       request: searchValue,
       sortBy: "default",
       maxAmount: 12,
-    }); // how to render Form.item https://github.com/ant-design/ant-design/issues/22421#issuecomment-778403975
+    });
     setIsModalVisible(true);
   };
 
@@ -83,43 +83,31 @@ const VideoCards = (props) => {
     let user = "";
     Object.keys(localStorage).forEach((item) => {
       if (item.startsWith("user")) {
-        console.log("user is = ", item);
-        //  let user = `user_${item}`;
         user = item;
       }
     });
     return user !== "" ? user : null;
   };
 
-  const onFinish = (values) => {
-    let admin = {
-      favourites: [
-        {
-          request: "request",
-          name: "How I want to name it",
-          sortBy: "default",
-          maxAmount: 12,
-          uniqueID: counter,
-        },
-      ],
-      token: "token_string",
-    };
-
-    // Логика добавления запросов в избранное
+  const getCounter = () => {
     let user = getUser();
-    console.log("user = ", user);
-    // Если мы тут, то юзер уже должен быть, по этому проверка не нужна и сразу берм данные из localstorage
+    let counter = 0;
+    JSON.parse(localStorage.getItem(user)).favourites.forEach((item, index) => {
+      if (item.uniqueID > counter) {
+        counter = item.uniqueID;
+      }
+    });
+    return counter;
+  };
+
+  const onFinish = (values) => {
+    let user = getUser();
+    let counter = getCounter() + 1;
     let localStorageValues = JSON.parse(localStorage.getItem(user));
     let clone = Object.assign(values, { uniqueID: counter });
-    console.log("clone = ", clone);
-    console.log("localStorageValues = ", localStorageValues);
-    // localStorageValues.favourites.push(clone);
 
-    // console.log("Form values = ", values);
-
-    // setCounter((prevValue) => prevValue + 1);
-
-    // Логика добавления запросов в избранное
+    localStorageValues.favourites.push(clone);
+    localStorage.setItem(user, JSON.stringify(localStorageValues));
     form.resetFields();
     setIsModalVisible(false);
   };
@@ -144,7 +132,6 @@ const VideoCards = (props) => {
           onCancel={handleCancel}
           okText="Сохранить"
           okButtonProps={{
-            // form: { form },
             key: "submit",
             htmlType: "submit",
           }}
